576fd325ab6837851ead10eb3c54fe2147a07b25
iamPraneetKaur <itspraneetkaur@gmail.com>
2025-06-23 17:40:12 +0530

    Base Frame

--- MainFrame.java
+++ MainFrame.java
@@ -4,8 +4,8 @@ class MainFrame{
     public static void main(String args[]){
         JFrame frame = new JFrame("Expense Tracker");
         frame.setLocationRelativeTo(null);
-        frame.setSize(600, 600);  gi
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
