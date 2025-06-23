a94ab73ec3ce1a752051bc2f3a1c85103583b253
iamPraneetKaur <itspraneetkaur@gmail.com>
2025-06-23 17:39:04 +0530

    Initial Commit: base frame

---
+++ MainFrame.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+class MainFrame{
+    public static void main(String args[]){
+        JFrame frame = new JFrame("Expense Tracker");
+        frame.setLocationRelativeTo(null);
+        frame.setSize(600, 600);  gi
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
