67d330677f1c3a904242663ae29010f9d36aa324
iamPraneetKaur <itspraneetkaur@gmail.com>
2025-06-24 14:18:32 +0530

    Creating additon panel frame

--- MainFrame.java
+++ MainFrame.java
@@ -1,8 +1,8 @@
 import java.awt.*;
 import javax.swing.*;
-
 class MainFrame {
     public static void main(String args[]) {
+        Font subLabel=  new Font("Arial", Font.BOLD, 16);
         JFrame frame = new JFrame("Expense Tracker");
         frame.setSize(600, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,43 +17,89 @@ class MainFrame {
 
         // Income Label
         JLabel incomeLabel = new JLabel("Total Income:");
-        incomeLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        incomeLabel.setFont(subLabel);
         incomeLabel.setBounds(50, 80, 150, 25);
         frame.add(incomeLabel);
+        //Income text field
+        JTextField inTextField= new JTextField();
+        inTextField.setBounds(200,  80, 150, 25);
+        frame.add(inTextField);
+
 
         // Expense Label
         JLabel expenseLabel = new JLabel("Total Expenses:");
-        expenseLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        expenseLabel.setFont(subLabel);
         expenseLabel.setBounds(50, 120, 150, 25);
         frame.add(expenseLabel);
+        //Expense Text Field
+        JTextField exTextField= new JTextField();
+        exTextField.setBounds(200,  120, 150, 25);
+        frame.add(exTextField);
 
         // Balance Label
         JLabel balanceLabel = new JLabel("Remaining Balance:");
-        balanceLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        balanceLabel.setFont(subLabel);
         balanceLabel.setBounds(50, 160, 180, 25);
         frame.add(balanceLabel);
+        //Balance Text Field
+        JTextField balTextField= new JTextField();
+        balTextField.setBounds(230,  160, 150, 25);
+        frame.add(balTextField);
 
         // Category Label
         JLabel categoryLabel = new JLabel("Expense Category:");
-        categoryLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        categoryLabel.setFont(subLabel);
         categoryLabel.setBounds(50, 200, 180, 25);
         frame.add(categoryLabel);
+        //Category Combo Box
+        String[] category={"","Household & Living", "Food", "Transportation", "Health", "Shopping/Personal", "Travel", "Education", "Work", "Financial", "Miscellaneous"};
+        JComboBox<String> catComboBox= new JComboBox<>(category);
+        catComboBox.setBounds(230, 200, 180, 25);
+        frame.add(catComboBox);
 
         // Date Label
         JLabel dateLabel = new JLabel("Date:");
-        dateLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        dateLabel.setFont(subLabel);
         dateLabel.setBounds(50, 240, 150, 25);
         frame.add(dateLabel);
+        //Date Combo Box
+        //days
+        Integer[] day = new Integer[31];
+        for (int i = 0; i < 31; i++) {
+            day[i] = i + 1;
+        }
+        JComboBox<Integer> dayComboBox= new JComboBox<>(day);
+        dayComboBox.setBounds(200, 240, 50, 25);
+        frame.add(dayComboBox);
+        //months
+        String month[]= {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        JComboBox<String> monthComboBox= new JComboBox<>(month);
+        monthComboBox.setBounds(250, 240, 100, 25);
+        frame.add(monthComboBox);
+        //year
+        Integer[] year = new Integer[16];
+        for (int i = 0; i < 16; i++) {
+            year[i] = 2020+i;
+        }      
+        JComboBox<Integer> yearComboBox= new JComboBox<>(year);
+        yearComboBox.setBounds(350, 240, 52, 25);
+        frame.add(yearComboBox);
+
 
         // Description Label
         JLabel descriptionLabel = new JLabel("Description:");
-        descriptionLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        descriptionLabel.setFont(subLabel);
         descriptionLabel.setBounds(50, 280, 150, 25);
         frame.add(descriptionLabel);
+        //Description Text Field
+        //Income text field
+        JTextField desTextField= new JTextField();
+        desTextField.setBounds(200,  80, 150, 25);
+        frame.add(desTextField);
 
         // Amount Label
         JLabel amountLabel = new JLabel("Amount:");
-        amountLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        amountLabel.setFont(subLabel);
         amountLabel.setBounds(50, 320, 150, 25);
         frame.add(amountLabel);
 
